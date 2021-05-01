d6afecd6b1f717ca1a801f57c4913902647bc3e6
Daniel Seiler <daniel.seiler@stud.tu-darmstadt.de>
2021-05-01 14:07:20 +0200

    Introduce ESC_* constants for logging

--- src/utils/Logging.h
+++ src/utils/Logging.h
@@ -14,6 +14,13 @@
 #include <iostream>
 #include <string>
 
+#define ESC_NORMAL "\e[0m"
+#define ESC_DIM "\e[2m"
+#define ESC_UNSET "\e[39m"
+#define ESC_RED "\e[91m"
+#define ESC_BLUE "\e[94m"
+#define ESC_YELLOW "\e[93m"
+
 namespace rdma {
 
 class Logging {
@@ -31,7 +38,7 @@ class Logging {
 
   static void error(string filename, int line, string msg) {
     if (Config::LOGGING_LEVEL <= 4)
-      log("\e[91m[ERROR]:\e[39m ", filename, line, msg);
+      log(ESC_RED "[ERROR]:" ESC_UNSET " ", filename, line, msg);
   }
 
   static void errorNo(string filename, int line, char* errorMsg, int errNo) {
@@ -42,23 +49,23 @@ class Logging {
 
   static void fatal(string filename, int line, string msg) {
     if (Config::LOGGING_LEVEL <= 5)
-      log("[FATAL]: ", filename, line, msg);
+      log(ESC_RED "[FATAL]:" ESC_UNSET " ", filename, line, msg);
     exit(1);
   }
 
   static void info(string msg) {
     if (Config::LOGGING_LEVEL <= 2)
-      log("\e[94m[INFO]:\e[39m  ", msg);
+      log(ESC_BLUE "[INFO]:" ESC_UNSET "  ", msg);
   }
 
   static void warn(string msg) {
     if (Config::LOGGING_LEVEL <= 3)
-      log("\e[93m[WARN]:\e[39m  ", msg);
+      log(ESC_YELLOW "[WARN]:" ESC_UNSET "  ", msg);
   }
  private:
   static void log(string type, string filename, int line, string msg) {
     cerr << type << msg << endl
-         << " at \e[2m" << filename << ":" << line << "\e[0m" << endl;
+         << " at " ESC_DIM << filename << ":" << line << ESC_NORMAL << endl;
   }
 
   static void log(string type, string msg) {
